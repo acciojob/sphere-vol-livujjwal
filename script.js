40bbec30f3d1d7e43990b90b3fa2c22e3178851a
Ujjwal Singh <132872642+livujjwal@users.noreply.github.com>
2023-08-20 07:46:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
 
+
+function volume_sphere() {
+ const radiusElement = document.getElementById('radius');
+ let volumeElement = document.getElementById('volume');
+	let r = radiusElement.value;
+	volumeElement.value = (4*r*r*r*Math.PI)/3;
+
+} 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
